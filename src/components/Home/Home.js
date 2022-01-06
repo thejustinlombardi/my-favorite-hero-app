c5e7f798cda1335751a5fc2369fabbe323491b9a
thejustinlombardi <thejustinlombardi@gmail.com>
2022-01-06 18:42:46 -0500

    Add links to footer

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,11 +6,10 @@ function Home(props) {
 			<div className="welcome-div">
 				<h2>Welcome, Heroes!</h2>
 				<p>
-					This site is designed for fellow heroes who are wanting more
-					information on their favorite heroes!
+					This site is designed for all who are wanting more information on
+					their favorite heroes!
 				</p>
-				<p> Click the HEROES tab above and search for your favorite hero!</p>
-				<p>Click on their individual card to find out all about them!</p>
+				<p> Click the `HEROES` tab above and search for your favorite hero!</p>
 			</div>
 
 			<div className="home-card">
